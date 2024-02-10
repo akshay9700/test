92a5bf29c3672fcc043e8671692a6ec193f6e7af
akshay9700 <110522215+akshay9700@users.noreply.github.com>
2024-02-10 14:30:52 +0530

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-yum install https -y
-systemctl httpd start
+yum install httpd -y
+service httpd start
 chkconfig httpd on 
 mkdir /var/www/html
 echo "hi AWS developers one" > /var/www/html/index.html
